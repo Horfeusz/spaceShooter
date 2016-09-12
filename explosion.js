8bf32bb08b1b800e8b9c09eb855ad32c2e72a02b
Michał Hoffmann <michalh@atena.pl>
2016-09-12 09:03:38 +0200

    Zmiejszyłem rozmiar ekslozji

--- explosion.js
+++ explosion.js
@@ -7,15 +7,21 @@ var FILE_COUNT = 278;
 
 function createExplosion(px, py) {
     
-    console.log("ekslozja x: " + px + " y: " + py);
+    var explosion = createEntity('explosion0000', {incFile: 0});
+
+    //Ustawiam wielkość ekslozji, zmniejszam rysunek o połowę
+    explosion.correctWidth = (explosion.img.width / 2) * -1;
+    explosion.correctHeight = (explosion.img.height / 2) * -1;                                                  
     
-    var explosion = createEntity('explosion0000', {x: px,
-                                                   y: py,
-                                                   incFile: 0});
+    //Ustawiam współrzędne ekslozji
+    explosion.x = px - (explosion.img.width / 4); 
+    explosion.y = py - (explosion.img.height / 4);
+
     //Nadpisanie metody poruszającej obiekt
     explosion.move = function() {        
         //Incrementuje indeks obrazka
         this.incFile++;
+        //this.incFile = this.incFile + 3;
         if(this.incFile > FILE_COUNT) {
             this.dead = true;
             return;
